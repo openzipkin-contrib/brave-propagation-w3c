d2924e5e842266a700a100a9bb1efc73e32efe39
adriancole <adrian.f.cole@gmail.com>
2020-12-18 14:18:04 +0800

    Adds tracestate validation
    As the currently released spec is a bit crap, this goes with @anuraaga's
    revision https://github.com/w3c/trace-context/pull/411

--- tracecontext/src/test/java/brave/propagation/tracecontext/TracestateFormatTest.java
+++ tracecontext/src/test/java/brave/propagation/tracecontext/TracestateFormatTest.java
@@ -29,6 +29,7 @@ class TracestateFormatTest {
 
   static final String LONGEST_BASIC_KEY =
     TWO_HUNDRED_FORTY_KEY_CHARS + FORTY_KEY_CHARS.substring(0, 16);
+  static final String LONGEST_VALUE = LONGEST_BASIC_KEY;
 
   static final String LONGEST_TENANT_KEY =
     "1" + TWO_HUNDRED_FORTY_KEY_CHARS + "@" + FORTY_KEY_CHARS.substring(0, 13);
@@ -90,6 +91,66 @@ class TracestateFormatTest {
 
   AbstractThrowableAssert<?, ? extends Throwable> assertThatThrownByValidateKey(String key) {
     return assertThatThrownBy(() -> tracestateFormat.validateKey(key, 0, key.length()))
+      .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test void validateValue_empty() {
+    assertThatThrownByValidateValue("")
+      .hasMessage("Invalid value: empty");
+  }
+
+  @Test void validateValue_tooLong() {
+    char[] tooMany = new char[257];
+    Arrays.fill(tooMany, 'a');
+    assertThatThrownByValidateValue(new String(tooMany))
+      .hasMessage("Invalid value: too large");
+  }
+
+  @Test void validateValue_specialCharacters() {
+    for (char allowedSpecial : Arrays.asList('@', '_', '~', '*', '/')) {
+      assertThatValidateValue(allowedSpecial + "").isTrue();
+      assertThatValidateValue("a" + allowedSpecial).isTrue();
+      assertThatValidateValue("a" + allowedSpecial + allowedSpecial).isTrue();
+      assertThatValidateValue("a" + allowedSpecial + "1").isTrue();
+    }
+  }
+
+  @Test void validateValue_spaces() {
+    assertThatThrownByValidateValue(" ")
+      .hasMessage("Invalid value: must end in a non-space character");
+    assertThatThrownByValidateValue("a ")
+      .hasMessage("Invalid value: must end in a non-space character");
+    assertThatValidateValue(" a").isTrue();
+    assertThatValidateValue("a a").isTrue();
+    assertThatValidateValue(" a a").isTrue();
+  }
+
+  @Test void validateValue_longest() {
+    assertThatValidateValue(LONGEST_VALUE).isTrue();
+  }
+
+  @Test void validateValue_shortest() {
+    for (char n = '0'; n <= '9'; n++) {
+      assertThatValidateValue(String.valueOf(n)).isTrue();
+    }
+    for (char l = 'a'; l <= 'z'; l++) {
+      assertThatValidateValue(String.valueOf(l)).isTrue();
+    }
+  }
+
+  @Test void validateValue_invalid_unicode() {
+    assertThatThrownByValidateValue("a💩")
+      .hasMessage("Invalid value: valid characters are: ' ' to '~', except ',' and '='");
+    assertThatThrownByValidateValue("💩a")
+      .hasMessage("Invalid value: valid characters are: ' ' to '~', except ',' and '='");
+  }
+
+  AbstractBooleanAssert<?> assertThatValidateValue(String value) {
+    return assertThat(tracestateFormat.validateValue(value, 0, value.length()));
+  }
+
+  AbstractThrowableAssert<?, ? extends Throwable> assertThatThrownByValidateValue(String value) {
+    return assertThatThrownBy(() -> tracestateFormat.validateValue(value, 0, value.length()))
       .isInstanceOf(IllegalArgumentException.class);
   }
 }
